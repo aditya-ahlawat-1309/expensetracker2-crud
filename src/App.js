707feadb8cceb91ef329f9e1ee66851a92436aa7
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2021-12-09 15:08:14 +0530

    expense tracker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Execution from "./Section-2_5/Vid-52/Execution"
+
+function App() {
+  return (
+    <div className="App">
+     <Execution/>
+    </div>
+  );
+}
+
+export default App;
