ee56b70fe51d9ea5e974c75e6eed8a91809ce13f
aditya <67224103+aditya-ahlawat-1309@users.noreply.github.com>
2021-12-20 12:17:17 +0530

    Update Execution.js

--- src/Section-2_5/Vid-52/Execution.js
+++ src/Section-2_5/Vid-52/Execution.js
@@ -1,50 +1,126 @@
-import React, { useState } from "react";
-
+import React from "react";
+import {useState, useEffect } from "react";
+import Card from "../UI/Card";
+import axios from "axios";
 import NewExpense from "../Vid-52/NewExpense";
 import Expenses from "../Vid-36/Expenses";
+import "./Execution.css"
 
 const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
+  
 ];
 
 const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+   //const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
+const [expenses, setExpenses] = useState([]);
+  const [expenseList, setExpenseList] = useState([]);
+const [newTitle, setNewTitle] = useState("");
+const [newAmount, setNewAmount] = useState("");
+  useEffect(() => {
+    axios.get("https://resumeexpensetracker.herokuapp.com/read/").then((response) => {
+      console.log(response);
+      setExpenseList(response.data);
+      setExpenses(response.data);
     });
+  }, []);
+
+  const updateExpense = (id) => {
+    axios.put("https://resumeexpensetracker.herokuapp.com/update/", {
+      id: id,
+      newTitle: newTitle,
+      newAmount: newAmount,
+    });
+    window.location.reload(true);
   };
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+  const deleteExpense = (id) => {
+    axios.delete(`https://resumeexpensetracker.herokuapp.com/delete/${id}`);
+    window.location.reload(true);
+  };
+
+
+  
+  const addExpenseHandler = (expense) => {
+    setExpenses((expenseList) => {
+      return [expense, ...expenseList];
+    }); 
+    window.location.reload(true);  
+  };
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
+
+      <li className="mern-contents-expense-tracker">
+        <h1 className="expense-item__heading">Mongo DB Contents</h1>
+
+        {expenseList.map((val, key) => {
+          return (
+            <div>
+              <div
+                style={{ display: "flex", padding: "1%", marginLeft: "19.5%" }}
+              >
+                <div class="expense-date-mern">{val.date}</div>
+                <div className="expense-item__description">
+                  <h2 style={{ position: "absolute", marginLeft: "5%" }}>
+                    {val.title}
+                  </h2>
+
+                  <div
+                    className="expense-item__price"
+                    style={{ position: "absolute", marginLeft: "23%" }}
+                  >
+                    ${val.amount}
+                  </div>
+
+                  <button
+                    onClick={() => deleteExpense(val._id)}
+                    className="expense-item__delete"
+                    style={{ position: "absolute", marginLeft: "35%" }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+
+              <label style={{ fontWeight: "bold" }}>UPDATED TITLE : </label>
+              <input
+                onChange={(event) => {
+                  setNewTitle(event.target.value);
+                  console.log(event.target.value);
+                }}
+                type="text"
+                placeholder="Please Enter Title"
+                className="input-text-expense"
+              />
+              {/* <input
+                onChange={(event) => {
+                  setNewAmount(event.target.value);
+                  console.log(event.target.value);
+                }}
+                type="number"
+                min="0.01"
+                placeholder="Please Enter Amount"
+                className="input-text-expense"
+              /> */}
+
+              <button
+                className="expense-item__update"
+                onClick={() => updateExpense(val._id)}
+              >
+                Update
+              </button>
+              <br />
+              <br />
+              <hr style={{width:"80%",height:"0.75px",backgroundColor:"white"}}/>
+
+              <br />
+            </div>
+          );
+        })}
+      </li>
     </div>
   );
 };
